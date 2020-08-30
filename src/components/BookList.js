6e70ef12829e894f28ab7edbe53c357a04503727
letsandeepio <sandeepchopra7@gmail.com>
2020-08-30 11:06:20 -0400

    adds styles

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+  render() {
+    return (
+      <div className="book-list">
+        <ul>
+          <li>the way of kinds</li>
+          <li>the name of the kings</li>
+          <li>Harry Potter</li>
+        </ul>
+      </div>
+    );
+  }
+}
