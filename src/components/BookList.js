f26f05de26ee16511bb885a48908e9b504cc4d2f
letsandeepio <sandeepchopra7@gmail.com>
2020-08-30 11:33:49 -0400

    adds theme context to NavBar & BookList

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <div className="book-list">
+      <div
+        className="book-list"
+        style={{ background: theme.ui, color: theme.syntax }}
+      >
         <ul>
           <li>the way of kinds</li>
           <li>the name of the kings</li>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 export default class NavBar extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
     return (
-      <nav>
+      <nav style={{ background: theme.ui, color: theme.syntax }}>
         <h1>Read-it-later App</h1>
         <ul>
           <li>Home</li>
