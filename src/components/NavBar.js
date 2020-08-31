080186573193c51a4186668b4de669aee59bfa29
letsandeepio <sandeepchopra7@gmail.com>
2020-08-31 08:58:20 -0400

    adds onclick handler to user auth

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,9 @@ export default class NavBar extends Component {
               const theme = isLightTheme ? light : dark;
               return (
                 <nav style={{ background: theme.ui, color: theme.syntax }}>
-                  <div>{isAuthenticated ? 'Logged in' : 'Logged out'}</div>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? 'Logged in' : 'Logged out'}
+                  </div>
                   <h1>Read-it-later App</h1>
                   <ul>
                     <li>Home</li>
